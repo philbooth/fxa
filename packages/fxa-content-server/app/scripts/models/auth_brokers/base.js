b839177a88e3574743f169f3b3e7131fc5c8950a
Vlad Filippov <vlad.filippov@gmail.com>
2019-10-21 14:00:01 -0400

    fix(links): stop converting links to text on Firefox iOS
    Fixes #3047

--- packages/fxa-content-server/app/scripts/models/auth_brokers/base.js
+++ packages/fxa-content-server/app/scripts/models/auth_brokers/base.js
@@ -550,10 +550,6 @@ const BaseAuthenticationBroker = Backbone.Model.extend({
      */
     browserTransitionsAfterEmailVerification: true,
     /**
-     * Should external links be converted to text?
-     */
-    convertExternalLinksToText: false,
-    /**
      * Is the email-first flow supported?
      */
     emailFirst: false,
